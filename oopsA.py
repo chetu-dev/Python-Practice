d47cfe6449ac3385bec329fcc778a3f0dde93ad0
Chetna Sarode <chetnasarode575@gmail.com>
2026-03-17 22:03:12 +0530

    oops 2 nd question solved

--- oopsA.py
+++ oopsA.py
@@ -3,18 +3,33 @@
 # Classes and Objects: 
 # 1. Create a class Person with attributes name and age. 
 class Person:
-    def display(self):
-        print("Name")
-        print("Age")
+    name = "Chetna"
+    age = 24
 p1 = Person()
-p1.display()
+print("Name :",p1.name)
+print("Age :",p1.age)
+print()
 
 # 2. Create two objects of the Person class and print their details. 
+class Person1:
+    pass
+obj1 = Person1()
+obj1.name = "Abhi"
+obj1.age = 24
+
+obj2 = Person1
+obj2.name = "Chetu"
+obj2.age = 24
+
+print("obj1:",obj1.name,obj1.age)
+print("obj2:",obj2.name,obj2.age)
 
 
 # 3. Add a method display() to print the object details. 
 # 4. Update the age of a person object. 
 # 5. Delete an attribute of a person object. 
+
+
 # Constructor & Self: 
 # 6. Create a class Student with a constructor to initialize name and marks. 
 # 7. Print student details using self. 
